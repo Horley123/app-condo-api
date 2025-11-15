72fe62d0b6c513e992b8af18e66c640f79706962
horley.monteiro <horley.monteiro@trixti.com.br>
2025-11-15 20:54:59 -0300

    init 2

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000, () => {
-    const url = `http://localhost:${process.env.PORT ?? 3000}`;
-    console.log(`🚀 API rodando em: ${url}`);
+    console.log(
+      `Server is running url   http://localhost:${process.env.PORT ?? 3000}`,
+    );
   });
 }
 bootstrap();
